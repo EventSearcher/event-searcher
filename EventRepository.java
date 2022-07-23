85a8b6fda304fafd710ab6297948b424dce94f1c
Mateuszpeco <97780228+Mateuszpeco@users.noreply.github.com>
2022-07-23 11:43:26 +0200

    Delete EventRepository.java

--- EventRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.sda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-
-    Event findByCity(String city);
-}
