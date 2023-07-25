966611259fea6f102f9b121cffbbdb2c1679fd18
Mateusz Satala <matsatala1@gmail.com>
2023-07-25 09:21:17 +0200

    quick fix v2 of schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,19 +2,16 @@
 create table IF NOT EXISTS USERS(
     id serial primary key,
     username VARCHAR(255) not null,
-    password VARCHAR(255),not null,
-    role int,
-    foreign key (role) references USERS_ROLE(role_id));
+    password VARCHAR(255) not null);
 --ROLE
 create table If NOT EXISTS ROLE(
-    id int primary key,
+    id integer primary key,
     name VARCHAR(255) not null);
 --USERS_ROLE
 create table IF NOT EXISTS USERS_ROLE(
-    user_id int not null,
-    role_id int not null,
-    foreign key (role_id) references ROLE(id),
-    foreign key (user_id) references USERS(id));
+    user_id integer references USERS(id),
+    role_id integer references ROLE(id),
+    CONSTRAINT users_roles_pk primary key(user_id,role_id));
 
 INSERT INTO role VALUES (1,'USER');
 INSERT INTO role VALUES (2,'ADMIN');
